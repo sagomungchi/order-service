328195bf6b69aa3ce23c620eba5c6ed58ef9db0f
SeanSin <seansin@chungbuk.ac.kr>
2020-07-07 22:39:41 +0900

    feat: item order & cart function

--- src/main/java/com/pcbang/order/mvp/cart/CartController.java
+++ src/main/java/com/pcbang/order/mvp/cart/CartController.java
@@ -1,4 +1,15 @@
 package com.pcbang.order.mvp.cart;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carts/")
 public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
 }
--- src/main/java/com/pcbang/order/mvp/cart/CartRepository.java
+++ src/main/java/com/pcbang/order/mvp/cart/CartRepository.java
@@ -1,4 +1,7 @@
 package com.pcbang.order.mvp.cart;
 
-public interface CartRepository {
+import com.pcbang.order.mvp.domain.cart.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
